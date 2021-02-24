0b48551f0682a86c94b4b367f1881b0bcda995eb
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2021-02-24 12:24:49 +0800

    Added code for the  lesson19_obligatory

--- script/script.js
+++ script/script.js
@@ -37,6 +37,55 @@ window.addEventListener('DOMContentLoaded', function () {
     }, 1000);
   }
 
-  countTimer('23 February 2021')
+  countTimer('23 February 2021');
+
+  // menu
+  const toggleMenu = () => {
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
+    
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
+    }
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+  };
+  toggleMenu();
+
+  //popup
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+      popupBtn = document.querySelectorAll('.popup-btn'),
+      popUpClose = document.querySelector('.popup-close');
+    let interval,
+      count = 0,
+      screenWidth = screen.width;
+    
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        popup.style.display = 'block';
+        if (screenWidth > 768) {
+          popup.style.opacity = 0;
+          interval = requestAnimationFrame(elementAnimate);
+          interval = 0;
+          count = 0;
+        }
+      })
+    });
+    popUpClose.addEventListener('click', () => { popup.style.display = 'none' });
+    
+    //animation
+    let elementAnimate = function() {
+      interval = requestAnimationFrame(elementAnimate);
+      count++;
+      if ((count / 100) < 1) {
+        popup.style.opacity = (count / 100);
+      }
+    }
+  };
+  togglePopUp();
 
 });
