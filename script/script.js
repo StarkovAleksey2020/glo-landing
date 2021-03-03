9807ef595ab3e61dee487af376c9bc75d55f6917
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2021-03-03 10:59:19 +0800

    Added code for the lesson24_obligatory

--- script/script.js
+++ script/script.js
@@ -357,4 +357,49 @@ window.addEventListener('DOMContentLoaded', function () {
 
   blurHandler();
 
+  // calculator
+  const calc = (price = 100) => {
+
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcDay = document.querySelector('.calc-day'),
+      calcCount = document.querySelector('.calc-count'),
+      totalValue = document.getElementById('total');
+    let calcTypeIndex;
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value / 10,
+      squareValue = +calcSquare.value;
+  
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value >= 5 && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (typeValue && squareValue) {
+        total = typeValue * squareValue * price * countValue * dayValue;
+      }
+      
+      totalValue.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', (event) => {
+      const target = event.target;
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+
+    });
+  };
+
+  calc(100);
 });
